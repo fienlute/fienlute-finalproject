e5963dd92066a02a8c0373a172f10f4584b4d7db
fienlute <fienlute@gmail.com>
2017-01-10 13:28:13 +0100

    Initial Commit

---
+++ GOals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GOals
+//
+//  Created by Fien Lute on 10-01-17.
+//  Copyright © 2017 Fien Lute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
