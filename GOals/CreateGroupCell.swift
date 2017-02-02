9769a91f8db27bb6a06637fe5d9d0a26cb2d6a4b
fienlute <fienlute@gmail.com>
2017-02-02 10:47:46 +0100

    opschoon

--- GOals/CreateGroupCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CreateGroupCell.swift
-//  
-//
-//  Created by Fien Lute on 19-01-17.
-//
-//
-
-import Foundation
--- GOals/CustumCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CustumCell.swift
-//  GOals
-//
-//  Created by Fien Lute on 13-01-17.
-//  Copyright © 2017 Fien Lute. All rights reserved.
-//
-
-import Foundation
